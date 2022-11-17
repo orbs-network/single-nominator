b17e8b1c3fb1fdb86870e8f330f8f6111112b00d
shlomizaig <shlomi.zayag@gmail.com>
2022-11-17 15:07:31 +0200

    - add missing print-hex - add balance checking before deployment

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -17,21 +17,25 @@ async function deploy() {
 		console.log(`contract: ${contract.address.toFriendly()} already Deployed`);
 		return contract;
 	}
+	const balance = await client.getBalance(deployWallet.address);
+	if ( balance.lte(toNano(NOMINATOR_MIN_TON)) ) {
+		throw `insfucient funds to deploy single nominator contract wallet have only ${balance}`;
+	}
 
 	const seqno = await deployWallet.getSeqNo();
 	const transfer = await deployWallet.createTransfer({
-	secretKey: deployWalletKey.secretKey,
-	seqno: seqno,
-	sendMode: 1 + 2,
-	order: new InternalMessage({
-	  to: contract.address,
-	  value: toNano(NOMINATOR_MIN_TON),
-	  bounce: false,
-	  body: new CommonMessageInfo({
-		stateInit: new StateInit({data: contract.source.initialData, code: contract.source.initialCode}),
-		body: null,
-	  }),
-	}),
+		secretKey: deployWalletKey.secretKey,
+		seqno: seqno,
+		sendMode: 1 + 2,
+		order: new InternalMessage({
+			to: contract.address,
+			value: toNano(NOMINATOR_MIN_TON),
+			bounce: false,
+			body: new CommonMessageInfo({
+				stateInit: new StateInit({data: contract.source.initialData, code: contract.source.initialCode}),
+				body: null,
+			}),
+		}),
 	});
 
 	await client.sendExternalMessage(deployWallet, transfer);
